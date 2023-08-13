3c0af28e547c0a04e274a1b57609d9abe87fc4fd
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-08-13 12:52:10 +0600

    Initial Commit

---
+++ JsonParsing/JsonParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonParsingApp.swift
+//  JsonParsing
+//
+//  Created by Auto on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct JsonParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
